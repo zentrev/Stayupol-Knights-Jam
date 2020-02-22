7ab005a05bd7c0b4988c7175c52a1a663b20a334
Rowan <emilyremy@gmail.com>
2020-02-22 10:31:46 -0700

    Added Supporting Scripts

---
+++ Assets/Scripts/Supporting/Health.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] Slider healthbar = null;
+    public float healthMax = 1.0f;
+    public float healthCount = 0.0f;
+
+    public int objectIndex = 0;
+
+    Animator an = null;
+
+    public void Start()
+    {
+        an = GetComponent<Animator>();
+        healthCount = healthMax;
+        if (healthbar)
+        {
+            healthbar.maxValue = healthMax;
+        }
+    }
+
+    public void ResetHealth()
+    {
+        healthCount = healthMax;
+    }
+
+    public void DealDamage(float dmg)
+    {
+        healthCount -= dmg;
+        if (an) an.SetTrigger("Damage");
+    }
+
+    public void FixedUpdate()
+    {
+
+        if (healthbar)
+        {
+            healthbar.maxValue = healthMax;
+            healthbar.value = healthCount;
+            healthbar.transform.rotation = Quaternion.identity;
+        }
+
+        if (healthCount > healthMax)
+        {
+            healthCount = healthMax;
+        }
+    }
+}
