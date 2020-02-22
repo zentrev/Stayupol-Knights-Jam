785bf819e3e941db5b008d87e0e63520918e6c58
Yoannyz <38017282+Yoannyz@users.noreply.github.com>
2020-02-22 14:35:39 -0700

    Modifiers

--- Assets/Scripts/Weaponry/Modifiers/ThreeWayModifier.cs
+++ Assets/Scripts/Weaponry/Modifiers/ThreeWayModifier.cs
@@ -4,8 +4,51 @@ using UnityEngine;
 
 public class ThreeWayModifier : ModifierBase
 {
+    [SerializeField] float m_distanceApart = 3;
+    private Rigidbody rb;
+    private bool spawn = true;
+    public bool horizontalShot = true;
+
     protected override void OnCollisionEnter(Collision collision)
     {
-        throw new System.NotImplementedException();
+        m_projectile.Destruct();
+    }
+
+    private void OnEnable()
+    {
+        m_projectile.TryGetComponent(out rb);
+    }
+
+    private void Start()
+    {
+
+        if (spawn)
+        {
+            if (horizontalShot)
+            {
+                ProjectileBase firstSpawn = Instantiate(m_projectile);
+                firstSpawn.GetComponent<ThreeWayModifier>().spawn = false;
+                firstSpawn.transform.position += new Vector3(-m_distanceApart, 0.0f, 0.0f).normalized;
+                firstSpawn.GetComponent<Rigidbody>().velocity = rb.velocity;
+
+                ProjectileBase secondSpawn = Instantiate(m_projectile);
+                secondSpawn.GetComponent<ThreeWayModifier>().spawn = false;
+                secondSpawn.transform.position += new Vector3(m_distanceApart, 0.0f, 0.0f).normalized;
+                secondSpawn.GetComponent<Rigidbody>().velocity = rb.velocity;
+            }
+            else
+            {
+                ProjectileBase firstSpawn = Instantiate(m_projectile);
+                firstSpawn.GetComponent<ThreeWayModifier>().spawn = false;
+                firstSpawn.transform.position += new Vector3(0.0f, -m_distanceApart, 0.0f).normalized;
+                firstSpawn.GetComponent<Rigidbody>().velocity = rb.velocity;
+
+                ProjectileBase secondSpawn = Instantiate(m_projectile);
+                secondSpawn.GetComponent<ThreeWayModifier>().spawn = false;
+                secondSpawn.transform.position += new Vector3(0.0f, m_distanceApart, 0.0f).normalized;
+                secondSpawn.GetComponent<Rigidbody>().velocity = rb.velocity;
+            }
+        }
+
     }
 }
